691946d66ce376c0ee16c549403be745101c8f9f
yuyinghan@yeah.net <yuyinghan@yeah.net>
2018-05-13 15:27:36 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>Hello world!!!</h1>
+    );
+  }
+}
+console.log(<Hello/>);
+ReactDOM.render(<Hello/>,document.getElementById("root"));
