b558c1e49c9f82683d220ccb5d75630cb07fe9e7
yuyinghan@yeah.net <yuyinghan@yeah.net>
2018-05-27 16:39:36 +0800

    完成https://reactjs.org/docs/components-and-props.html中的Extracting的例子

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render() {
+const comment={date:new Date(),text:'神评论666',author:{name:"tomli",url:'http://placekitten.com/g/64/64'}};
+
+class Avatar extends  React.Component{
+    render(){
+     return (<img src={this.props.user.url} alt={this.props.user.name}/>);
+
+    }
+}
+class UserInfo extends  React.Component{
+  render(){
     return (
-      <h1>Hello world123!!</h1>
-    );
+        <div className="user-info">
+        <Avatar user={this.props.author}/>
+          <div>{this.props.author.name}</div>
+        </div>
+    )
   }
 }
-console.log(<Hello/>);
-ReactDOM.render(<Hello/>,document.getElementById("root"));
+
+
+class Comment extends  React.Component{
+  render(){
+    return (
+      <div className="comment">
+        <UserInfo author={this.props.comment.author}/>
+        <div>{this.props.comment.text}</div>
+        <div>{this.props.comment.date.toLocaleString()}</div>
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<Comment comment={comment}/>,document.getElementById("root"));
