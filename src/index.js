7355a0981f5c03d058c645bb4171607623795d6a
yuyinghan@yeah.net <yuyinghan@yeah.net>
2018-05-27 19:19:17 +0800

    实践state及生命周期问题

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={date:new Date()};
+  }
+  componentDidMount(){
+    this.tid=setInterval(()=>{this.tick()},1000)
+  }
+  componentWillUnmount(){
+      clearInterval(tid);
+  }
+  tick(){
+    this.setState({date:new Date()});
+  }
   render() {
     return (
-      <h1>Hello world123!!</h1>
+      <h1>Hello world123!!{this.state.date.toLocaleString()}</h1>
     );
   }
 }
-console.log(<Hello/>);
+
 ReactDOM.render(<Hello/>,document.getElementById("root"));
